d9a0231104b808559d8e2adebd45db6ba51af2a8
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-10-18 21:15:12 -0400

    making-maps

--- client-app/components/molecules/SendOreder/sendOrder.tsx
+++ client-app/components/molecules/SendOreder/sendOrder.tsx
@@ -1,23 +1,25 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Button, Accordion, AccordionItem, RadioGroup, Radio } from '@nextui-org/react'
-import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
 
 type Props = {
-    goToStep: (e:number) => void
-    currentStepIndex: number
-}
+  goToStep: (e: number) => void;
+  currentStepIndex: number;
+};
+
 function SendOrder ({ goToStep, currentStepIndex }: Props) {
   const [selectedKeys, setSelectedKeys] = useState(new Set(['0']))
   const [showAlert, setShowAlert] = useState(false)
-  const [activeDirection, setActiveDirection] = useState({ lat: 0, lng: 0 })
+  const [activeDirection, setActiveDirection] = useState({ lat: -17.414, lng: -66.1653 })
   const send = useRef({
     type: '',
     address: ''
   })
 
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: 'AIzaSyDiFHyCkH5AcfAMHRNvzlZ2CpPzxQMXBns' || ''
+    googleMapsApiKey: 'AIzaSyDcrnsyWSi4kyUOyUujyL0zhmuOfyubZ9U' || ''
   })
+
   const onSetDirection = (marker: any) => {
     const { lat, lng } = marker.latLng
     setActiveDirection({
@@ -25,6 +27,12 @@ function SendOrder ({ goToStep, currentStepIndex }: Props) {
       lng: lng()
     })
   }
+
+  // Función para actualizar las coordenadas cuando cambian en el estado
+  useEffect(() => {
+    // Puedes realizar acciones aquí cuando las coordenadas cambian en el estado
+    console.log('Coordenadas actualizadas:', activeDirection)
+  }, [activeDirection])
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
@@ -36,61 +44,61 @@ function SendOrder ({ goToStep, currentStepIndex }: Props) {
     setShowAlert(false)
     handleNext()
   }
+
   const handleNext = () => {
     goToStep(currentStepIndex + 1)
   }
+
   const handlePlace = (e: any) => {
-    send.current = ({
-      type: e.target.name === 'Entrega a domicilo' ? 'Entrega a domicilo' : 'Recoger de la sucursal',
+    send.current = {
+      type: e.target.name === 'Entrega a domicilio' ? 'Entrega a domicilio' : 'Recoger de la sucursal',
       address: e.target.value
-    })
-    console.log(send.current)
+    }
   }
 
   return (
     <div className='h-full w-full p-5 flex flex-col justify-between text-center space-y-5 '>
       <div className='px-8 overflow-y-auto'>
-      <h2 className='text-gray-500 md:pb-6'>Datos de ubicación</h2>
-      <Accordion
-        selectedKeys={selectedKeys}
-        onSelectionChange={setSelectedKeys as any }
-      >
-        <AccordionItem key="1" aria-label="Recoger de la sucursal" title="Recoger de la sucursal">
-          <RadioGroup onChange={(e) => handlePlace(e) } color='secondary'>
-            <Radio value='Sucursal América'>Sucursal América</Radio>
-            <Radio value='Sucursal Circunvalación'>Sucursal Circunvalación</Radio>
-            <Radio value='Sucursal laguna'>Sucursal laguna</Radio>
-          </RadioGroup>
-        </AccordionItem>
-        <AccordionItem key="2" aria-label="Entrega a domicilo" title="Entrega a domicilo">
-        <div className='w-full'>{isLoaded &&
-                      <GoogleMap
-                        onClick={onSetDirection}
-                        center={activeDirection}
-                        zoom={17}
-                        mapContainerStyle={{ width: '100%', height: '100%' }}
-                      >
-                        <MarkerF
-                          position={activeDirection}
-                        />
-                      </GoogleMap>
-                    }</div>
-        </AccordionItem>
-      </Accordion>
+        <h2 className='text-gray-500 md:pb-6'>Datos de ubicación</h2>
+        <Accordion selectedKeys={selectedKeys} onSelectionChange={setSelectedKeys as any}>
+          <AccordionItem key='1' aria-label='Recoger de la sucursal' title='Recoger de la sucursal'>
+            <RadioGroup onChange={(e) => handlePlace(e)} color='secondary'>
+              <Radio value='Sucursal América'>Sucursal América</Radio>
+              <Radio value='Sucursal Circunvalación'>Sucursal Circunvalación</Radio>
+              <Radio value='Sucursal laguna'>Sucursal laguna</Radio>
+            </RadioGroup>
+          </AccordionItem>
+          <AccordionItem key='2' aria-label='Entrega a domicilio' title='Entrega a domicilio'>
+            <div className='w-full h-96'>
+              {isLoaded && (
+                <GoogleMap
+                  onClick={onSetDirection}
+                  center={activeDirection}
+                  zoom={17}
+                  mapContainerStyle={{ width: '100%', height: '100%' }}
+                >
+                  <Marker position={activeDirection} />
+                </GoogleMap>
+              )}
+            </div>
+          </AccordionItem>
+        </Accordion>
       </div>
-      { showAlert && <div className='flex  justify-center items-center flex-col'>
-        <p className='text-red-500'>Por favor, seleccione una opción</p>
-      </div>
-      }
-      <div className='flex justify-between px-6  items-center '>
-          <Button onClick={() => goToStep(currentStepIndex - 1)} color='primary' className='w-1/4' >
-            Atrás
-          </Button>
-          <Button onClick={(e) => handleSubmit(e)} color='primary' className='w-1/4' >
-            Siguiente
-          </Button>
+      {showAlert && (
+        <div className='flex justify-center items-center flex-col'>
+          <p className='text-red-500'>Por favor, seleccione una opción</p>
         </div>
+      )}
+      <div className='flex justify-between px-6  items-center '>
+        <Button onClick={() => goToStep(currentStepIndex - 1)} color='primary' className='w-1/4'>
+          Atrás
+        </Button>
+        <Button onClick={(e) => handleSubmit(e)} color='primary' className='w-1/4'>
+          Siguiente
+        </Button>
       </div>
+    </div>
   )
 }
+
 export default SendOrder
--- client-app/pages/order/index.tsx
+++ client-app/pages/order/index.tsx
@@ -1,14 +1,32 @@
 import OrderLayout from '@/components/templates/OrderLayout/orderLayout'
 import ClientLayout from '@/components/templates/ClientLayout/ClientLayout'
+import { useAppSelector } from '@/components/redux/hooks'
+
+import InformationCard from '@/components/molecules/Card/InformationCard'
 
 function Order () {
-  return (
-    <div className="h-full">
-        <ClientLayout>
-          <OrderLayout />
-        </ClientLayout>
-    </div>
-  )
+  const cartItems = useAppSelector((state) => state.cartReducer.initialState.cartItems)
+  if (cartItems.length === 0) {
+    return (
+      <div className="h-full">
+          <ClientLayout>
+            <InformationCard
+            title='¡No tienes productos en tu carrito!'
+            description='Agrega productos a tu carrito para poder realizar tu compra.'
+            buttonText='Regresar a la tienda'
+            buttonDestination='/' />
+          </ClientLayout>
+      </div>
+    )
+  } else {
+    return (
+      <div className="h-full">
+          <ClientLayout>
+            <OrderLayout />
+          </ClientLayout>
+      </div>
+    )
+  }
 }
 
 export default Order
