79a8bbb04a0cf1ab4fe4d11468ceb7490a04eae2
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-11-28 15:13:08 -0400

    last commit from administration-panel

--- client-app/components/templates/layouts/sidebar/index.tsx
+++ client-app/components/templates/layouts/sidebar/index.tsx
@@ -36,7 +36,7 @@ const Sidebar: React.FC<TSidebarProps> = ({
       <div onClick={() => setSidebar(false)} className={`fixed inset-0 bg-primary bg-opacity-50 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} aria-hidden="true"></div>
       <div className={`fixed max-h-screen z-50 flex left-0 top-0 h-screen text-white transition-all duration-700 ${isSidebarOpen ? 'w-80 -translate-x-20' : 'w-16 xl:w-[70px] -translate-x-20 md:-translate-x-0'}`}>
         {/* Simple sidebar */}
-        <div className={`w-32 min-w-[64px] h-full flex flex-col justify-between bg-primary  ${!isSidebarOpen ? 'rounded-tr-2xl rounded-br-2xl' : ''}`}>
+        <div className={`w-28 min-w-[64px] h-full flex flex-col justify-between bg-primary  ${!isSidebarOpen ? 'rounded-tr-2xl rounded-br-2xl' : ''}`}>
           <div>
             <Image alt='100' width={50} height={20} src='/../../common/logo (1).png' className='mt-20 mb-4 h-16 w-16   px-1 bg-white rounded-full object-contain m-auto'/>
             <div>
