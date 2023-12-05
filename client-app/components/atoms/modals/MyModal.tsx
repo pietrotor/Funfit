1b026676941a52a5e1e943c52549640d9bd699f6
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-12-05 16:49:33 -0400

    adding  edit functionality

--- client-app/components/atoms/modals/MyModal.tsx
+++ client-app/components/atoms/modals/MyModal.tsx
@@ -5,6 +5,7 @@ export type ModalProps = {
   isOpen: boolean
   onClose: () => void
   children?: React.ReactNode
+  hideCloseButton?: boolean
   size?:
     | 'xs'
     | 'sm'
@@ -22,10 +23,11 @@ export const MyModal = ({
   isOpen,
   onClose,
   children,
+  hideCloseButton = true,
   size = '2xl'
 }: ModalProps) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} className="w-full" size={size}>
+    <Modal isOpen={isOpen} onClose={onClose} className="w-full" hideCloseButton={hideCloseButton} size={size} >
       <ModalContent>{close => <>{children}</>}</ModalContent>
     </Modal>
   )
